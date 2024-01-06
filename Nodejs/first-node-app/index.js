9bbe05215171656fcba0028507f15b158cec3d13
han18 <han.fadah@gmail.com>
2024-01-05 19:59:21 -0500

    this is creating my first Node server

--- Nodejs/first-node-app/index.js
+++ Nodejs/first-node-app/index.js
@@ -1,21 +1,27 @@
 // this is running JS outside the browser
 console.log("Hello Node!");
-console.log("Installed npm i --save-dev nodemon!");
+console.log("Installed npm i --save-dev nodemon and other dependency!");
 console.log("nodemon keeps refreshing the file when updating the server!");
 
+//this module transfer data over the Hyper Text Transfer Protocol
 const http = require("http");
 
+// creating the ports
 const hostname = "127.0.0.1";
 const port = 3000;
 
+// creating my first server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World Calling From a Node Server!\n");
+  res.setHeader("Content-Type", "text/html");
+  res.write('<h1 style="color: red">Hello World!</h1>');
+  res.write("<p>I wonder what else we can send...</p>");
+  res.end();
 });
 
-res.status = server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// listening for communicating
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}: and ${port}/`);
 });
 
 // this is a test
