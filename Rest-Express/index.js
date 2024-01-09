560b36d97962fb8016784f3fc98be242f0352812
han18 <han.fadah@gmail.com>
2024-01-09 10:20:12 -0500

    Using the CRUD method to request and response to data

--- Rest-Express/index.js
+++ Rest-Express/index.js
@@ -7,9 +7,27 @@ const posts = require("./data/post");
 const app = express();
 const PORT = 3006;
 
-// the is a middleware for a REST API must be set up
-app.use(express.json());
+// this middleware for a REST API that must be set up all time to work for different data body
+app.use(express.json()); // this is control & brings data in
 app.use(express.urlencoded({ extended: true }));
+
+// New logging middleware to help us keep track of
+// requests during testing!
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+    ${time.toLocaleTimeString()}: Received a ${req.method} request to ${
+      req.url
+    }.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
 
 // the original main route
 app.get("/", (req, res) => {
@@ -20,10 +38,11 @@ app.get("/", (req, res) => {
  * Creating a GET route for the entire users database.
    This would be impractical in larger data sets sent to the user in json.
  */
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
+// app.get("/api/users", (req, res) => {
+//   res.json(users);
+// });
 
+// to read the users
 app
   .route("/api/users")
   .get((req, res) => {
@@ -32,17 +51,17 @@ app
   .post((req, res) => {
     if (req.body.name && req.body.username && req.body.email) {
       if (users.find((u) => u.username == req.body.username)) {
-        res.json({ error: "User is taken" });
+        res.json({ error: "Username is taken" });
         return;
       }
-
+      // create a new user object
       const user = {
-        id: users[users.length - 1].id + 1,
+        id: users[users.length - 1].id + 1, // increasing the user id
         name: req.body.name,
         username: req.body.username,
         email: req.body.email,
       };
-
+      // pushing the new user
       users.push(user);
       res.json(users[users.length - 1]);
     } else res.json({ error: "insufficient data" });
@@ -51,27 +70,76 @@ app
 /**
  * @method GET /api/:id
  * @param :id
+ * getting single ids enter numbers in router
  */
-app.get("/api/users/:id", (req, res, next) => {
-  const user = users.find((u) => u.id == req.params.id);
-  if (user) res.json(user);
-  else next(); // to call the error
-});
+app
+  .route("/api/users/:id")
+  .get((req, res, next) => {
+    const user = users.find((u) => u.id == req.params.id);
+    if (user) res.json(user);
+    else next(); // to call the error
+  })
+  .patch((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        for (const key in req.body) {
+          users[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  })
+  .delete((req, res, next) => {
+    // The DELETE request route simply removes a resource.
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        users.splice(i, 1);
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  });
 
 // ============= post method ==========
 /**
- * this is a post route
+ * Get /api/posts main post API route
  */
-app.get("/api/posts", (req, res) => {
-  res.json(posts);
-});
+app
+  .route("/api/posts")
+  .get((req, res) => {
+    res.json(posts);
+  })
+  .post((req, res) => {
+    // Within the POST request route, we create a new
+    // post with the data given by the client.
+    if (req.body.userId && req.body.title && req.body.content) {
+      const post = {
+        id: posts[posts.length - 1].id + 1,
+        userId: req.body.userId,
+        title: req.body.title,
+        content: req.body.content,
+      };
 
-app.get("/api/posts/:id", (req, res) => {
+      posts.push(post);
+      res.json(posts[posts.length - 1]);
+    } else res.json({ error: "Insufficient Data" });
+  });
+
+///////
+
+// GET all posts adding next parameter for error
+app.get("/api/posts/:id", (req, res, next) => {
   const post = posts.find((p) => p.id == req.params.id);
   if (post) res.json(post);
+  else next();
 });
 
-// this is to catch a wrong route
+// 404 this is to catch a wrong route must add next
 app.use((req, res, next) => {
   res.status(404);
   res.json({ error: "Resource, Page, Route Not Found" });
